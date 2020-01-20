21a625955fee7469c09d3a8024ef4cd98258b0d9
Pierre-Marie de Rodat <derodat@adacore.com>
2020-01-20 12:28:09 +0100

    Rewrite ptrfinder programs to use Libadalang.Helpers.App

--- src/ptrfinder1.adb
+++ src/ptrfinder1.adb
@@ -1,78 +1,94 @@
-with Ada.Text_IO;           use Ada.Text_IO;
+with Ada.Strings.Fixed;
+with Ada.Text_IO; use Ada.Text_IO;
+
+with Langkit_Support.Slocs;
 with Libadalang.Analysis;   use Libadalang.Analysis;
 with Libadalang.Common;     use Libadalang.Common;
-with Ada.Strings.Fixed;     use Ada.Strings.Fixed;
-with Ada.Strings;           use Ada.Strings;
-with Langkit_Support.Slocs; use Langkit_Support.Slocs;
+with Libadalang.Helpers;
 
 procedure ptrfinder1 is
 
-   procedure Detection
-     (Filename    : String;
-      Line_Number : Langkit_Support.Slocs.Line_Number) is
-   begin
-      Put_Line (Trim (Filename & ":" & Line_Number'Img,Left));
-   end Detection;
+   package Helpers renames Libadalang.Helpers;
+   package LAL renames Libadalang.Analysis;
+   package Slocs renames Langkit_Support.Slocs;
 
-   LAL_CTX  : constant Analysis_Context := Create_Context;
+   procedure Report (Node : LAL.Ada_Node'Class);
+   --  Report the use of an access type at Filename/Line_Number on the standard
+   --  output.
+
+   procedure Process_Unit
+     (Job_Ctx : Helpers.App_Job_Context; Unit : LAL.Analysis_Unit);
+   --  Look for the use of access types in Unit
+
+   package App is new Helpers.App
+     (Name         => "ptrfinder1",
+      Description  => "Look for the use of access types in the input sources",
+      Process_Unit => Process_Unit);
+
+   ------------
+   -- Report --
+   ------------
+
+   procedure Report (Node : LAL.Ada_Node'Class) is
+      Filename : constant String := Node.Unit.Get_Filename;
+      Line     : constant Slocs.Line_Number :=
+         Node.Sloc_Range.Start_Line;
+   begin
+      Put_Line (Filename & ":"
+                & Ada.Strings.Fixed.Trim (Line'Image, Ada.Strings.Left));
+   end Report;
+
+   ------------------
+   -- Process_Unit --
+   ------------------
+
+   procedure Process_Unit
+     (Job_Ctx : Helpers.App_Job_Context; Unit : LAL.Analysis_Unit)
+   is
+      pragma Unreferenced (Job_Ctx);
+
+      function Process_Node (Node : Ada_Node'Class) return Visit_Status;
+      --  Callback for LAL.Traverse
+
+      ------------------
+      -- Process_Node --
+      ------------------
+
+      function Process_Node (Node : Ada_Node'Class) return Visit_Status is
+      begin
+         case Node.Kind is
+            when Ada_Base_Type_Decl =>
+               if Node.As_Base_Type_Decl.P_Is_Access_Type then
+                  Report (Node);
+               end if;
+
+            when Ada_Object_Decl =>
+               if Node.As_Object_Decl.F_Type_Expr
+                  .P_Designated_Type_Decl.P_Is_Access_Type
+               then
+                  Report (Node);
+               end if;
+
+            when others =>
+
+               --  Nothing interesting was found in this Node so continue
+               --  processing it for other violations.
+
+               return Into;
+         end case;
+
+         --  A violation was detected, skip over any further processing of this
+         --  node.
+
+         return Over;
+      end Process_Node;
+
+   begin
+      if not Unit.Has_Diagnostics then
+         Unit.Root.Traverse (Process_Node'Access);
+      end if;
+   end Process_Unit;
 
 begin
-
-   Read_Standard_Input:
-   while not End_Of_File(Standard_Input)
-   loop
-
-      Process_Ada_Unit:
-      declare
-
-         Filename : constant String := Get_Line;
-
-         Unit : constant Analysis_Unit := LAL_CTX.Get_From_File(Filename);
-
-         function Process_Node(Node : Ada_Node'Class) return Visit_Status is
-         begin
-
-            case Node.Kind is
-
-               when Ada_Base_Type_Decl =>
-
-                  if Node.As_Base_Type_Decl.P_Is_Access_Type then
-
-                     Detection (Filename,Node.Sloc_Range.Start_Line);
-
-                  end if;
-
-               when Ada_Object_Decl =>
-
-                  if Node.As_Object_Decl.F_Type_Expr.
-                       P_Designated_Type_Decl.P_Is_Access_Type then
-
-                    Detection (Filename,Node.Sloc_Range.Start_Line);
-
-                  end if;
-
-               when others =>
-
-                  return Into;
-                  --   Nothing interesting was found in this Node so
-                  --   continue processing it for other violations
-
-            end case;
-
-            return Over;
-            --   A violation was detected,  skip over any further processing
-            --   of this Node.
-
-         end Process_Node;
-
-      begin
-
-         if not Unit.Has_Diagnostics then
-            Unit.Root.Traverse (Process_Node'Access);
-         end if;
-
-      end Process_Ada_Unit;
-
-   end loop Read_Standard_Input;
-
+   App.Run;
 end ptrfinder1;
--- src/ptrfinder2.adb
+++ src/ptrfinder2.adb
@@ -1,55 +1,157 @@
-with Ada.Text_IO;       use Ada.Text_IO;
-with Ada.Directories;   use Ada.Directories;
-with Ada.Strings;       use Ada.Strings;
-with Ada.Strings.Fixed; use Ada.Strings.Fixed;
-with Ada.IO_Exceptions;
+with Ada.Strings.Fixed;
+with Ada.Text_IO; use Ada.Text_IO;
+
+with GNATCOLL.Opt_Parse;
+with Langkit_Support.Slocs;
+with Libadalang.Analysis;   use Libadalang.Analysis;
+with Libadalang.Common;     use Libadalang.Common;
+with Libadalang.Helpers;
 
 procedure ptrfinder2 is
-begin
 
-   Read_Standard_Input:
-   while not End_Of_File(Standard_Input)
-   loop
+   package Helpers renames Libadalang.Helpers;
+   package LAL renames Libadalang.Analysis;
+   package Slocs renames Langkit_Support.Slocs;
 
-      Process_Standard_Input:
-      declare
+   function Verify
+     (Filename : String; Line : Slocs.Line_Number) return Boolean;
+   --  Return whether Filename can be read and that its Line'th line contains
+   --  the " access " substring.
 
-         Std_Input : constant String := Get_Line;
-         Delimeter_Position : constant Natural := Index(Std_Input,":");
-         Line_Number_As_String : constant String :=  Std_Input(Delimeter_Position+1..Std_Input'Last);
-         Line_Number : constant Integer := Integer'Value(Line_Number_As_String);
-         Filename : constant String := Std_Input(Std_Input'First..Delimeter_Position-1);
-         The_File : File_Type;
-         Verified : Boolean := False;
+   procedure Report (Node : LAL.Ada_Node'Class);
+   --  Report the use of an access type at Filename/Line_Number on the standard
+   --  output. If --verify is enabled, check that the first source line
+   --  corresponding to Node contains the " access " substring.
 
-      begin
+   procedure Process_Unit
+     (Job_Ctx : Helpers.App_Job_Context; Unit : LAL.Analysis_Unit);
+   --  Look for the use of access types in Unit
 
-         if Ada.Directories.Exists(Filename) and then Line_Number > 1
-         then
+   package App is new Helpers.App
+     (Name         => "ptrfinder2",
+      Description  => "Look for the use of access types in the input sources",
+      Process_Unit => Process_Unit);
 
-            Open(File => The_File, Mode => In_File, Name => Filename);
+   package Do_Verify is new GNATCOLL.Opt_Parse.Parse_Flag
+     (App.Args.Parser,
+      Long => "--verify",
+      Help => "Verify detected ""access"" occurences");
 
-            Locate_Line:
-            for I in 1..Line_Number loop
+   ------------
+   -- Verify --
+   ------------
 
-               Verified := Index(Get_Line(The_File)," access ") > 0;
+   function Verify
+     (Filename : String; Line : Slocs.Line_Number) return Boolean
+   is
+      --  Here, we could directly look for an "access" token in the list of
+      --  tokens corresponding to Line in this unit. However, in the spirit of
+      --  the original program, re-read the file with Ada.Text_IO.
 
-               exit when Verified or else End_Of_File(The_File);
+      Found : Boolean := False;
+      --  Whether we have found the substring on the expected line
 
-            end loop Locate_Line;
+      File : File_Type;
+      --  File to read (Filename)
+   begin
+      Open (File, In_File, Filename);
+      for I in 1 .. Line loop
+         declare
+            use type Slocs.Line_Number;
 
-            Close(File => The_File);
+            Line_Content : constant String := Get_Line (File);
+         begin
+            if I = Line
+               and then Ada.Strings.Fixed.Index (Line_Content, " access ") > 0
+            then
+               Found := True;
+            end if;
+         end;
+      end loop;
+      Close (File);
+      return Found;
+   exception
+      when Use_Error | Name_Error | Device_Error =>
+         Close (File);
+         return Found;
+   end Verify;
 
-         end if;
+   ------------
+   -- Report --
+   ------------
 
-         if Verified then
-            Put_Line("Access Type Verified on line #" & Line_Number_As_String & " of " & Filename);
+   procedure Report (Node : LAL.Ada_Node'Class) is
+      Filename   : constant String := Node.Unit.Get_Filename;
+      Line       : constant Slocs.Line_Number :=
+         Node.Sloc_Range.Start_Line;
+      Line_Image : constant String :=
+         Ada.Strings.Fixed.Trim (Line'Image, Ada.Strings.Left);
+   begin
+      if Do_Verify.Get then
+         if Verify (Filename, Line) then
+            Put_Line ("Access Type Verified on line #"
+                      & Line_Image & " of " & Filename);
          else
-            Put_Line("Suspected Access Type *NOT* Verified on line #" & Line_Number_As_String & " of " & Filename);
+            Put_Line ("Suspected Access Type *NOT* Verified on line #"
+                      & Line_Image & " of " & Filename);
          end if;
 
-      end Process_Standard_Input;
+      else
+         Put_Line (Filename & ":" & Line_Image);
+      end if;
+   end Report;
 
-   end loop Read_Standard_Input;
+   ------------------
+   -- Process_Unit --
+   ------------------
 
+   procedure Process_Unit
+     (Job_Ctx : Helpers.App_Job_Context; Unit : LAL.Analysis_Unit)
+   is
+      pragma Unreferenced (Job_Ctx);
+
+      function Process_Node (Node : Ada_Node'Class) return Visit_Status;
+      --  Callback for LAL.Traverse
+
+      ------------------
+      -- Process_Node --
+      ------------------
+
+      function Process_Node (Node : Ada_Node'Class) return Visit_Status is
+      begin
+         case Node.Kind is
+            when Ada_Base_Type_Decl =>
+               if Node.As_Base_Type_Decl.P_Is_Access_Type then
+                  Report (Node);
+               end if;
+
+            when Ada_Object_Decl =>
+               if Node.As_Object_Decl.F_Type_Expr
+                  .P_Designated_Type_Decl.P_Is_Access_Type
+               then
+                  Report (Node);
+               end if;
+
+            when others =>
+
+               --  Nothing interesting was found in this Node so continue
+               --  processing it for other violations.
+
+               return Into;
+         end case;
+
+         --  A violation was detected, skip over any further processing of this
+         --  node.
+
+         return Over;
+      end Process_Node;
+
+   begin
+      if not Unit.Has_Diagnostics then
+         Unit.Root.Traverse (Process_Node'Access);
+      end if;
+   end Process_Unit;
+
+begin
+   App.Run;
 end ptrfinder2;
